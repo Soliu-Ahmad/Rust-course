b14b7c821cee19c9e4a6921d480776186b11eb25
MAC <mac@MACs-MacBook-Pro.local>
2025-08-27 23:25:21 +0100

    signed integer and unsigned integer in rust

--- helloProject/src/main.rs
+++ helloProject/src/main.rs
@@ -1,3 +1,28 @@
+// fn main() {
+//     println!("Hello, Rust from CARGO!");
+// }
+
+
+// premitive data Types 
+// int, float, bool, char
+
+// Integer
+//Rust has signed (+ and -) and Unsigned integer (only positive) types of different sizes.
+// i8, i16, i32, i64, i128: signed integers.
+// u8, u16, u32, u64, u128: unsigned integers.
+
 fn main() {
-    println!("Hello, Rust from CARGO!");
+    let x: i32 = 42;
+    let y: u64 = 100;
+
+    println!("Signed Integer: {}", x);
+    println!("Unsigned Integer: {}", y);
 }
+
+// Floating Point
+// f32, f64
+
+// Boolean
+// bool
+
+// Character
