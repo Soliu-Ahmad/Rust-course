85791ad057096eb56c759fc4c9a8dbde276a87f9
MAC <mac@MACs-MacBook-Pro.local>
2025-09-09 12:11:53 +0100

    everything about compound data type

--- helloProject/src/compound.rs
+++ helloProject/src/compound.rs
@@ -30,21 +30,55 @@ fn main() {
       println!("Age: {}", human.1);
       println!("Active: {}", human.2);
 
-
-        // Two types of Formatting:
-// 1. Display Formatting: {}
-// 2. Debug Formatting: {:?}
+      let my_mix_tuple = ("Kratos", 20, true, ["apple", "banana", "cherry", "mango"]);
+      println!("My Mix Tuple: {:?}", my_mix_tuple);
 
 
+      // Slice
+         let number_slice: &[i32] = &[1,2,3,4,5];
+         println!("Number Slice: {:?}", number_slice);
 
-    let name = "Alice";
-let age = 30;
-println!("Name: {}, Age: {}", name, age);
+         let animal_slice: &[&str] = &["dog", "cat", "bird", "fish"];
+         println!("Animal Slice: {:?}", animal_slice);
 
-println!("Name: {:?}, Age: {:?}", name, age);
+         let book_slice: &[&String] = &[&"IT".to_string(), &"The Great Gatsby".to_string(), &"The Catcher in the Rye".to_string()];
+         println!("Book Slice: {:?}", book_slice);
+
+
+         // String vs String Slice (&str)
+         // String [ growable, mutable, owned string  type]
+         // all variable in rust language are imutable by default
+         let mut stone_code = String::from("Stone Code");
+         
+         stone_code.push_str(" is a programming language");
+
+         println!("Stone Code: {}", stone_code);
+
+         // B- &str [String Slice]
+         let string: String = String::from("Hello, Worlds of Rust");
+         let slice: &str = &string[0..5];
+         println!("Slice value: {}", slice);
+
+
+         
+         
+         
+
+
+         
+
+            // Two types of Formatting:
+            // 1. Display Formatting: {}
+            // 2. Debug Formatting: {:?}
+
+
+
+            let name = "Alice";
+            let age = 30;
+            println!("Name: {}, Age: {}", name, age);
+
+            println!("Name: {:?}, Age: {:?}", name, age);
 }
-
-
 
 
 
