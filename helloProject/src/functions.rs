1116e54382df4f3458e01a2bf45d6e3661ce8d56
MAC <mac@MACs-MacBook-Pro.local>
2025-09-09 22:49:28 +0100

    everything about functions in rust

--- helloProject/src/functions.rs
+++ helloProject/src/functions.rs
@@ -3,27 +3,6 @@
 // // an function / variable should be written in snake case 
 // // snake case: for example hello_world
 // // rabab case: for example hello-world
-// fn main() {
-//     hello_world();
-//     tell_height(182);
-//     human_id(name: "Johe", age: 55 , height: 182.0);
-// }
-
-// // Hoisting can call function any where in ur code 
-
-// fn hello_world(){
-//     println!("Hello, Rust from Functions!");
-// }
-
-// // you can insert input values
-// fn tell_height(height: u32){
-//     println!("My height is {} cm.", height);
-// }
-
-// // You can insert more than one value 
-// fn human_id(name: &str, age: u32, height: f32){
-//     println!("My name is {}, I am {} years old, and my height is {} cm.", name, age, height);
-// }
 
 // Functions
 // Entry point of the program
@@ -35,7 +14,7 @@ fn main() {
     human_id("Johe", 55, 182.0);   // ✅ Correct function call
 
     let calculate_total: i32 = {
-        let price: i32 = 5;
+        let price: i32 = 5; 
         let qty: i32 = 10;
         price * qty
     };
@@ -44,6 +23,17 @@ fn main() {
     let y: i32 = add(4, 6);   // ✅ Fixed
     println!("Value of y is : {}", y);
     println!("Value from function 'add' is: {}.", add(4,6));
+
+    // Calling the BMI function
+   
+     // ✅ Declare before use
+        let weight: f64 = 70.0;
+        let height: f64 = 1.82;
+
+    let bmi: f64 = calculate_bmi(weight, height);
+    println!("Your BMI is: {:.2}", bmi);
+
+
 }
 
 // Hoisting means you can call a function anywhere in your code
@@ -72,9 +62,28 @@ fn add(a: i32, b: i32) -> i32 {
 // Expression and Statements 
 // Expression: Anything that returns a value.
 // Statement: Anything that does not return a value.
+// Almost all Statement  in Rust end with;
+// let y = let x = 10;
+
+// 1. variable decleration: let x = 6;
+// 2. function Definitions: fn fool() {}
+// 3. Control flow statements: if Condition {/* code */}
+// else {/* code */}, while Condition {
+//     {/* code */}, etc.
+// }
+
 
 // Expression
 //------------
 // true & false
 // add(3, 4)
 // if Conditions {value1} else {value2}
+
+
+// final example
+  
+
+// BMI = weight(kg) / height(m)^2
+fn calculate_bmi(weight_kg: f64, height_m: f64) -> f64 {
+    weight_kg / (height_m * height_m)
+}
